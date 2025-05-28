3e014d5ff4799f0b8d75c9845a00ee7fa771aa7a
luanamessias12 <luanamessiasestudante@gmail.com>
2025-05-27 21:08:36 -0300

    Corrigido redirect de busca

--- src/app/(ui)/search/page.tsx
+++ src/app/(ui)/search/page.tsx
@@ -4,26 +4,24 @@ import { SearchInput } from "@/components/ui/search-input";
 import { tweet } from "@/data/tweet";
 import { redirect } from "next/navigation";
 
-type Props = {
-    searchParams: { q?: string };
-    
-};
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { q?: string };
+}) {
+  if (!searchParams.q) redirect("/");
 
-export default  function Page ({ searchParams}: Props) {
-    if(!searchParams.q) redirect('/');
-
-    return (
-        <div>
-            <GeneralHeader backHref="/">
-                <SearchInput defaultValue={searchParams.q}/>
-            </GeneralHeader>
-            <div className="border-t-2 border-gray-900">
-                <TweetItem tweet={tweet} />
-                <TweetItem tweet={tweet} />
-                <TweetItem tweet={tweet} />
-                <TweetItem tweet={tweet} />
-            </div>
-        </div>
-    );
-
+  return (
+    <div>
+      <GeneralHeader backHref="/">
+        <SearchInput defaultValue={searchParams.q} />
+      </GeneralHeader>
+      <div className="border-t-2 border-gray-900">
+        <TweetItem tweet={tweet} />
+        <TweetItem tweet={tweet} />
+        <TweetItem tweet={tweet} />
+        <TweetItem tweet={tweet} />
+      </div>
+    </div>
+  );
 }

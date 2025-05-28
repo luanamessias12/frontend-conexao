4d0455e6a872b129c160673f4f2628d1c6105c66
luanamessias12 <luanamessiasestudante@gmail.com>
2025-05-28 10:56:03 -0300

    Ocultando temporariamente página com erro para permitir deploy

--- src/app/(ui)/search/page.tsx
+++ src/app/(ui)/search/page.tsx
@@ -1,29 +1,25 @@
-import { TweetItem } from "@/components/tweet/tweet-item";
-import { GeneralHeader } from "@/components/ui/general-header";
-import { SearchInput } from "@/components/ui/search-input";
-import { tweet } from "@/data/tweet";
-import { redirect } from "next/navigation";
+//import { TweetItem } from "@/components/tweet/tweet-item";
+//import { GeneralHeader } from "@/components/ui/general-header";
+//import { SearchInput } from "@/components/ui/search-input";
+//import { tweet } from "@/data/tweet";
+//import { redirect } from "next/navigation";
+//
+//export default function Page({ searchParams }: { searchParams?: Record<string, string | string[] | undefined> }) {
+ // const query = Array.isArray(searchParams?.q) ? searchParams.q[0] : searchParams?.q;
 
-type PageProps = {
-  searchParams?: {
-    q?: string;
-  };
-};
+  //if (!query) redirect("/");
 
-export default function Page({ searchParams }: PageProps) {
-  if (!searchParams?.q) redirect("/");
-
-  return (
-    <div>
-      <GeneralHeader backHref="/">
-        <SearchInput defaultValue={searchParams.q} />
-      </GeneralHeader>
-      <div className="border-t-2 border-gray-900">
-        <TweetItem tweet={tweet} />
-        <TweetItem tweet={tweet} />
-        <TweetItem tweet={tweet} />
-        <TweetItem tweet={tweet} />
-      </div>
-    </div>
-  );
-}
+ // return (
+    //<div>
+    //  <GeneralHeader backHref="/">
+      //  <SearchInput defaultValue={query} />
+      //</GeneralHeader>
+      //<div className="border-t-2 border-gray-900">
+       // <TweetItem tweet={tweet} />
+        //<TweetItem tweet={tweet} />
+        //<TweetItem tweet={tweet} />
+        //<TweetItem tweet={tweet} />
+      //</div>
+  //  </div>
+  //);
+//}
